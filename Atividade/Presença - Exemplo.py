a3ee4a54e00688be94e08a008a6820cd5ca25398
Dani Oliveira <86899014+D1L3L@users.noreply.github.com>
2023-09-25 13:57:04 -0300

    atividade de presença dia 28/09

---
+++ Atividade/Presença - Exemplo.py
@@ -0,0 +1,25 @@
+from aula06 import divisao
+import unittest
+from conceito_aula06 import multiplicar
+from conceito_aula06 import divisao
+from conceito_aula06 import soma
+
+class TestMultiplicar(TestCase):
+class TestMultiplicar(unittest.TestCase):
+
+    def test_multiplicar_dois_por_tres(self):
+        self.assertEqual(multiplicar(2,3), 6)
+
+class TestDivisao(TestCase):
+class TestDivisao(unittest.TestCase):
+
+    def test_divisao_dois_por_tres(self):
+        self.assertEqual(divisao(2,3), -1)
+
+class TestSoma(unittest.TestCase):
+
+    def test_soma_dois_por_tres(self):
+        self.assertEqual(soma(2,3), 5)
+
+if __name__ == "__main__":
+    unittest.main()
